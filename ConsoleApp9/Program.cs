5c83da65b95f384b4a5d8f5178953e8e2aa928ec
User <User@DESKTOP-64J817K>
2022-01-27 09:31:46 +0200

    Add project files.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+             int a = int.Parse(Console.ReadLine());
+                double b = Math.Sqrt(a);
+                Console.WriteLine(b);
+                if (b<0)
+                {
+                    throw new Exception();
+                }
+            }
+            
+            catch (FormatException)
+            {
+               Console.WriteLine("Invalid number!");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye!");
+            }
+        }
+    }
+}
